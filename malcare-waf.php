cb53abd995de82f0523399b37563e4268208ea54
Ardi <ardata.indonesia@gmail.com>
2024-05-18 14:49:52 +0700

    mita

---
+++ malcare-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/landingu/amidekorasi.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/landingu/amidekorasi.com/wp-content/mc_data/');
+	define("MCCONFKEY", '01df05f9f975f3a7e9461fcaf794065f');
+	include_once('/home/landingu/amidekorasi.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
